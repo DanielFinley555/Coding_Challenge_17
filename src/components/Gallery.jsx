f598c95e5ca397e1f0a68341c9601bce2bf8ae4b
John Doe <johndoe@email.com>
2024-11-22 14:34:41 -0600

    "Setup root component with global state and routing." And fixing Gallery.jsx so it works.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -8,19 +8,24 @@ const [tours, setTours] = useState([]);
 
 //Fetch data from the API using useEffect
 useEffect(() => {
-    fetch("https://course-api.com/react-tours-project")
+    //Had to copy and paste the link from the api website in order for the fetch to work
+    //so the link is 'https://www.course-api.com/react-tours-project', instead of 'https://course-api.com/react-tours-project'
+    fetch('https://www.course-api.com/react-tours-project')
         .then((response) => response.json())
         .then((data) => setTours(data));
-  }, []);
+}, []);
 
 //using map to render a list of the tours
   return (
     <div>
-        <h2>Daily Tours</h2>
+        <h2>Tourist Locations</h2>
         <ul>
-            {tours.map(tour => (
+            {tours.map((tour) => (
                 <li key={tour.id}>
-                    {tour.name}, ${tour.price}, {tour.description}, {tour.image}
+                    <img src={tour.image} alt={tour.name} />
+                    <h3>{tour.name}</h3>
+                    <h4>Price: ${tour.price}</h4>
+                    <p>Description: {tour.info}</p>
                 </li>
             ))}
         </ul>
@@ -34,4 +39,4 @@ export default ToursDashboard;
 //Not sure how to do these remaining steps yet:
 //using button to create an option to remove a tour from the list
 //using button to create an option to toggle visibility
-//using to display loading and error messages
+//using to display loading messages
